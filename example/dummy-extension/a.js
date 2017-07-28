5b0cef5b5587204079ac77ee119cb0c95cca2663
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2017-07-28 18:00:25 +0200

    Change dummy-extension

--- example/dummy-extension/a.js
+++ example/dummy-extension/a.js
@@ -1,3 +1,10 @@
 console.log('I\' m injected')
 var foo = "FOOOOO"
 // throw new Error('Foo')
+
+const imgURL = chrome.runtime.getURL('/ta_maman.png');
+
+var body = document.querySelector('body')
+var img = new Image();
+img.src = imgURL;
+body.appendChild(img);
--- example/dummy-extension/b.js
+++ example/dummy-extension/b.js
@@ -1,5 +1,6 @@
-console.log(foo);
+// console.log(foo);
 console.log(chrome.extension.getURL('/foo.png'))
+chrome.storage.sync.set({foo: 'fioo'}, function (v) {console.log(v)})
 chrome.storage.sync.get({foo: 'fioo'}, function (v) {console.log(v)})
 
 
