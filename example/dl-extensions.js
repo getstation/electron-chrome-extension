8632c7afd665c6d7a52d3aef867c06c016f345bc
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2017-07-31 18:13:51 +0200

    Add a script to DL chrome extensions for example

---
+++ example/dl-extensions.js
@@ -0,0 +1,20 @@
+const downloadCrx = require('download-crx');
+const unzip = require('unzip-crx');
+const tmp = require('tmp');
+const path = require('path');
+
+const EXTENSIONS = [
+  {
+    id: 'pgbdljpkijehgoacbjpolaomhkoffhnl',
+    slug: 'mailtracker'
+  }
+];
+const destinationFolder = path.resolve(__dirname, 'extensions');
+
+const tempDir = tmp.dirSync().name;
+EXTENSIONS.forEach(extension => {
+  downloadCrx
+    .downloadById(extension.id, tempDir, extension.slug)
+    .then(filePath => unzip(filePath, path.resolve(destinationFolder, extension.slug)))
+    .then(() => console.log(`DLed ${extension.slug}`))
+})
