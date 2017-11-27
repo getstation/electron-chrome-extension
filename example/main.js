72464cdd985552a4b95f42081f018c7b98dd5604
Hugo Mano <hugo@hugomano.com>
2017-11-27 12:33:54 +0100

    Implement calls in dummy extension

--- example/main.js
+++ example/main.js
@@ -43,9 +43,9 @@ function createWindow () {
 app.on('ready', createWindow)
 
 app.on('ready', () => {
-  extensions.addExtension(path.join(__dirname, './extensions/mixmax'))
+  // extensions.addExtension(path.join(__dirname, './extensions/mixmax'))
   // extensions.addExtension(path.join(__dirname, './extensions/gmelius'))
-  // extensions.addExtension(path.join(__dirname, './extensions/dummy-extension'))
+  extensions.addExtension(path.join(__dirname, './extensions/dummy-extension'))
 
   const filter = {
     urls: ['<all_urls>']
