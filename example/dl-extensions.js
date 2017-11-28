bf175aa8fdb56b96a6334129b442dfa10b820fc6
Hugo Mano <hugo@hugomano.com>
2017-11-28 12:44:18 +0100

    Add chromeStoreExtensionId

--- example/dl-extensions.js
+++ example/dl-extensions.js
@@ -2,52 +2,53 @@ const downloadCrx = require('download-crx');
 const unzip = require('unzip-crx');
 const tmp = require('tmp');
 const path = require('path');
+const fs = require('fs');
 
 const EXTENSIONS = [
-  // {
-  //   id: 'pgbdljpkijehgoacbjpolaomhkoffhnl',
-  //   slug: 'mailtracker'
-  // },
-  // {
-  //   id: 'kbfnbcaeplbcioakkpcpgfkobkghlhen',
-  //   slug: 'gramarly'
-  // },
-  // {
-  //   id: 'ocpljaamllnldhepankaeljmeeeghnid',
-  //   slug: 'mixmax'
-  // },
-  // {
-  //   id: 'hihakjfhbmlmjdnnhegiciffjplmdhin',
-  //   slug: 'rapportive'
-  // },
-  // {
-  //   id: 'pnnfemgpilpdaojpnkjdgfgbnnjojfik',
-  //   slug: 'streak'
-  // },
-  // {
-  //   id: 'gkjnkapjmjfpipfcccnjbjcbgdnahpjp',
-  //   slug: 'yesware'
-  // },
-  // {
-  //   id: 'pmnhcgfcafcnkbengdcanjablaabjplo',
-  //   slug: 'clearbit-connect'
-  // },
-  // {
-  //   id: 'dheionainndbbpoacpnopgmnihkcmnkl',
-  //   slug: 'gmelius'
-  // },
-  // {
-  //   id: 'ghbmnnjooekpmoecnnnilnnbdlolhkhi',
-  //   slug: 'google-drive-offline'
-  // },
-  // {
-  //   id: 'aomjjhallfgjeglblehebfpbcfeobpgk',
-  //   slug: '1password'
-  // },
-  // {
-  //   id: 'hdokiejnpimakedhajhdlcegeplioahd',
-  //   slug: 'lastpass'
-  // },
+  {
+    id: 'pgbdljpkijehgoacbjpolaomhkoffhnl',
+    slug: 'mailtracker'
+  },
+  {
+    id: 'kbfnbcaeplbcioakkpcpgfkobkghlhen',
+    slug: 'gramarly'
+  },
+  {
+    id: 'ocpljaamllnldhepankaeljmeeeghnid',
+    slug: 'mixmax'
+  },
+  {
+    id: 'hihakjfhbmlmjdnnhegiciffjplmdhin',
+    slug: 'rapportive'
+  },
+  {
+    id: 'pnnfemgpilpdaojpnkjdgfgbnnjojfik',
+    slug: 'streak'
+  },
+  {
+    id: 'gkjnkapjmjfpipfcccnjbjcbgdnahpjp',
+    slug: 'yesware'
+  },
+  {
+    id: 'pmnhcgfcafcnkbengdcanjablaabjplo',
+    slug: 'clearbit-connect'
+  },
+  {
+    id: 'dheionainndbbpoacpnopgmnihkcmnkl',
+    slug: 'gmelius'
+  },
+  {
+    id: 'ghbmnnjooekpmoecnnnilnnbdlolhkhi',
+    slug: 'google-drive-offline'
+  },
+  {
+    id: 'aomjjhallfgjeglblehebfpbcfeobpgk',
+    slug: '1password'
+  },
+  {
+    id: 'hdokiejnpimakedhajhdlcegeplioahd',
+    slug: 'lastpass'
+  },
   {
     id: 'fdjamakpfbbddfjaooikfcpapjohcfmg',
     slug: 'dashlane'
@@ -60,5 +61,6 @@ EXTENSIONS.forEach(extension => {
   downloadCrx
     .downloadById(extension.id, tempDir, extension.slug)
     .then(filePath => unzip(filePath, path.resolve(destinationFolder, extension.slug)))
+    .then(() => fs.writeFile(path.resolve(destinationFolder, extension.slug, 'chromeStoreExtensionId'), extension.id))
     .then(() => console.log(`DLed ${extension.slug}`))
 })
