f102cfb761800daf256c99e8a010cc6610d12935
Hugo Mano <hugo@hugomano.com>
2018-11-30 11:51:47 +0100

    TS setup

--- src/renderer/api/event.js
+++ src/renderer/api/event.js
@@ -1,29 +1,29 @@
 class Event {
-  constructor () {
+  constructor() {
     this.listeners = [];
   }
 
-  addListener (callback) {
+  addListener(callback) {
     this.listeners.push(callback);
   }
 
-  hasListener (callback) {
+  hasListener(callback) {
     return this.listeners.indexOf(callback) !== -1;
   }
 
-  removeListener (callback) {
+  removeListener(callback) {
     const index = this.listeners.indexOf(callback);
     if (index !== -1) {
       this.listeners.splice(index, 1)
     }
   }
 
-  hasListener (callback) {
+  hasListener(callback) {
     const index = this.listeners.indexOf(callback)
     return index !== -1
   }
 
-  emit (...args) {
+  emit(...args) {
     for (const listener of this.listeners) {
       listener(...args);
     }
