8fc981c7eedb9a7babe14371b6e4a6dc814e9044
Hugo Mano <hugo@hugomano.com>
2018-11-30 11:15:06 +0100

    Some fixes

--- example/main.js
+++ example/main.js
@@ -26,7 +26,6 @@ app.on('ready', () => {
   createWindow();
 
   require('electron-process-manager').openProcessManager();
-
   addExtension(join(__dirname, './extensions/mixmax'))
 });
 
@@ -45,4 +44,53 @@ app.on('activate', () => {
 app.on('session-created', session => {
   const userAgent = session.getUserAgent();
   session.setUserAgent(userAgent.replace(/Electron\/\S*\s/, ''))
+
+  // const filter = {
+  //   urls: [
+  //     'https://*.google.com/mail/u/0/*', 'chrome-extension://dheionainndbbpoacpnopgmnihkcmnkl'
+  //   ],
+  // }
+  // session.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+  //   console.log(details);
+  //   callback({ cancel: false, requestHeaders: details.requestHeaders });
+  // })
+
+  // Sample example of how disable cors
+  // const filter = {
+  //   urls: ['https://compose.mixmax.com/api/views/*']
+  // }
+
+  // session.webRequest.onHeadersReceived(filter, (details, callback) => {
+  //   if (details.resourceType === 'xhr') {
+  //     const { protocol, hostname, pathname } = new URL(details.url)
+
+  //     const responseHeaders = details.responseHeaders
+  //     const requestHeaders = details.headers
+  //     const updatedHeaders = {
+  //       ...responseHeaders,
+  //       'access-control-allow-credentials': responseHeaders['access-control-allow-credentials'] || ['true'],
+  //       'access-control-allow-headers': [].concat(
+  //         responseHeaders['access-control-allow-headers'],
+  //         requestHeaders['Access-Control-Request-Headers'],
+  //         Object.keys(requestHeaders).filter((k) => k.startsWith('X-'))
+  //       ),
+  //       'Access-Control-Allow-Origin': [
+  //         `chrome-extension://ocpljaamllnldhepankaeljmeeeghnid`
+  //       ]
+  //     }
+  //     console.log("updatedHeaders l1: ", details);
+  //     console.log("updatedHeaders l2: ", updatedHeaders);
+  //     return callback({ cancel: false, responseHeaders: updatedHeaders })
+  //   }
+  // })
+
+  // session.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+  //   if (details.resourceType === 'xhr') {
+  //     const a = {
+  //       ...details.requestHeaders,
+  //       'Origin': ['null']
+  //     }
+  //     callback({ cancel: false, requestHeaders: a })
+  //   }
+  // })
 });
