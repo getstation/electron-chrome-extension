b043e0334bc08f25524d7fb248df1c5dcd98c7b1
Hugo Mano <hugo@hugomano.com>
2018-12-05 13:44:17 +0100

    Clean and add __apiPath

--- src/renderer/chrome-api.ts
+++ src/renderer/chrome-api.ts
@@ -1,32 +1,50 @@
 import { Extension, ChromeApi } from '../shared/types';
-import { getApiScope, getTypeByReference, proxify } from './utils';
-import { api } from '../shared/definitions';
+import { getApi, getTypeByReference, resolve, addTargetApiPath } from './utils';
+import { apis } from '../shared/definitions';
 
 declare global {
   interface Window { chrome: ChromeApi; }
 }
+
+const { log, warn } = console;
+const targetProvider = { __apiPath: [], apis };
 
 const injectChromeApi = (
   context: Window,
   extensionId: Extension['id'],
   isBackgroundPage: boolean,
 ) => {
-  console.log('Inject Chrome API: ', extensionId, isBackgroundPage);
+  log(`Inject Chrome API for ${extensionId} ${isBackgroundPage}`);
 
   // Detect Chrome version
   // Detect if allowed in CS
   // Detect if permission present
+  // Type checking on call
 
   const handler = {
     get(target: any, property: PropertyKey, _: any): any {
-      console.log('--- proxy get: ', target, property);
+      const { __apiPath } = target;
+
+      if (typeof property !== 'string') {
+        warn('Property is not a string');
+        return;
+      }
 
       // API
 
-      const apiScope = getApiScope(String(property));
+      const apis = target['apis'];
 
-      if (apiScope) {
-        return new Proxy(apiScope, handler);
+      if (apis) {
+        const api = getApi(apis, property);
+
+        if (api) {
+          return new Proxy(addTargetApiPath(api, target, property), handler);
+        }
+
+        if (__apiPath.length === 0) {
+          warn(`chrome.${property} is missing`);
+          return;
+        }
       }
 
       // Sub namepaces and properties
@@ -38,9 +56,12 @@ const injectChromeApi = (
           const reference = properties[property]['$ref'];
 
           if (reference) {
-            return new Proxy(getTypeByReference(target, reference), handler);
+            const type = getTypeByReference(target, reference);
+
+            return new Proxy(addTargetApiPath(type, target, property), handler);
           }
 
+          // property
           return properties[property];
         }
       }
@@ -48,15 +69,17 @@ const injectChromeApi = (
       // Methods
 
       if (target['functions']) {
-        const method = proxify(target, 'functions', property);
+        const method = resolve(target, 'functions', property);
 
         if (method) {
           if (method['returns']) {
-            console.log('Function return immediatly: ', target['id'], method);
+            log([...__apiPath, method.name], method.parameters);
+            // synchronous method call
             return;
           }
 
-          console.log('Function with callback: ', method['parameters']);
+          log([...__apiPath, method.name], method.parameters);
+          // asynchronous method call
           return;
         }
       }
@@ -66,28 +89,26 @@ const injectChromeApi = (
       const eventMethods = ['addListener', 'hasListener', 'removeListener'];
 
       if (eventMethods.includes(String(property))) {
-        console.log('Event: ', target, property);
+        log([...__apiPath, property]);
         return;
       }
 
       const events = target['events'];
 
       if (events) {
-        const event = proxify(target, 'events', property);
-        return new Proxy(event, handler);
+        const event = resolve(target, 'events', property);
+        return new Proxy(addTargetApiPath(event, target, property), handler);
       }
 
 
       // Default Behavior
 
-      return target[property];
+      warn(` ${target}.${property} is not handled`);
     },
   };
 
-  // const callbacksStore = new Map<string, Function>();
-
   // Mute the Window object to add the chrome namespace
-  context.chrome = new Proxy({}, handler);
+  context.chrome = new Proxy(targetProvider, handler);
 
   return Object.freeze(context.chrome);
 };
--- src/renderer/utils.ts
+++ src/renderer/utils.ts
@@ -1,12 +1,22 @@
-import { apis } from '../shared/definitions';
-
 // Proxy
 
-export const getApiScope = (namespace: string) =>
-  apis.find((api: any) => api.namespace === namespace);
+export const getApi = (target: any, namespace: string) =>
+  target.find((api: any) => api.namespace === namespace);
 
 export const getTypeByReference = (target: any, reference: string) =>
   target['types'].find((t: any) => t.id === reference);
 
-export const proxify = (target: any, type: string, property: PropertyKey) =>
+export const resolve = (target: any, type: string, property: string) =>
   target[type].find((m: any) => m['name'] === property);
+
+export const addTargetApiPath = (target: any, originalTarget: any, path: string) => {
+  const { __apiPath } = originalTarget;
+
+  return Object.assign(
+    {},
+    target,
+    {
+      __apiPath: [...__apiPath, path],
+    },
+  );
+};
