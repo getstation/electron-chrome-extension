2ca0d92978b3ff9beb109bc19854b6c3efdc3e05
Mikaël <lorack@gmail.com>
2018-12-12 18:30:46 +0100

    Create draft CxFetcher class and its 2 default providers
    + storage default provider
    + downloader default provider

---
+++ src/browser/cx-fetcher/cx-fetcher.ts
@@ -0,0 +1,50 @@
+import EventEmitter = require('events');
+import defaultCxStorage from './cx-storage-provider';
+import defaultCxDownloader from './cx-download-provider';
+
+export default class CxFetcher extends EventEmitter {
+  // Hold the singleton instance
+  private static instance:CxFetcher;
+  public cxStorage:any;
+  public cxDownloader:any;
+
+  /**
+   * Construct the singleton, return the only instance if already created, build it if not
+   * @param cxStorage Dependence that will handle all file system storage
+   * @param cxDownloader Dependence that will handle download and external fetch
+   */
+  constructor(cxStorage?: any, cxDownloader?: any) {
+    // Let this be a singleton
+    if (CxFetcher.instance) {
+      return CxFetcher.instance;
+    }
+
+    // Never forget this guy
+    super();
+
+    // Registrer the downloader and storage handler
+    this.cxStorage = (cxStorage) ? cxStorage : defaultCxStorage;
+    this.cxDownloader = (cxDownloader) ? cxDownloader : defaultCxDownloader;
+
+    // Start auto-update
+    this.autoFetchUpdates();
+
+    CxFetcher.instance = this;
+  }
+
+  public async fetchOne(extensionId: string) {
+    const destFolder = this.cxStorage.getDestinationFolder('extension');
+    console.log('Dest folder : ', destFolder);
+    const filePath = await this.cxDownloader.downloadById(extensionId, destFolder);
+    console.log('DLed : ', extensionId);
+    return filePath;
+  }
+
+  public checkForUpdate() {
+
+  }
+
+  private autoFetchUpdates() {
+
+  }
+}
