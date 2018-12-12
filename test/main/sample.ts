b9dbbc0d55a1fb337efda503349d47824cb872ae
Mikaël <lorack@gmail.com>
2018-12-12 18:28:59 +0100

    Update testing suite loader and folder structure

--- test/main/sample.ts
+++
@@ -1,3 +0,0 @@
-import { ipcMain } from 'electron';
-
-ipcMain.on('test-me', (e: any) => e.returnValue = 'pong');
--- test/renderer/sample.ts
+++
@@ -1,9 +0,0 @@
-import assert = require('assert');
-import { ipcRenderer } from 'electron';
-
-describe('Sample Test Feature', () => {
-  it('Sample Test', () => {
-    const pong = ipcRenderer.sendSync('test-me');
-    assert(pong, 'pong');
-  });
-});
