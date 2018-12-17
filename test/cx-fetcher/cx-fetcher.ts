bac57ed1799590ae80fdf9af048dd1e7a0a74346
Mikaël <lorack@gmail.com>
2018-12-17 11:29:25 +0100

    Add first check for update test
    WIP

--- test/cx-fetcher/cx-fetcher.ts
+++ test/cx-fetcher/cx-fetcher.ts
@@ -3,6 +3,17 @@ import assert = require('assert');
 import CxFetcher from '../../src/browser/cx-fetcher/cx-fetcher';
 
 const EXTENSION_ID = 'dheionainndbbpoacpnopgmnihkcmnkl';
+const EXTENSION_UPDATE_URL = '';
+const EXTENSION_PATH = '';
+const FAKE_CX_INFOS = { version: '1.0.0', update_url: EXTENSION_UPDATE_URL, path: EXTENSION_PATH };
+const FAKE_UPDATE_XML = `
+<?xml version='1.0' encoding='UTF-8'?>
+<gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
+  <app appid='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'>
+    <updatecheck codebase='http://myhost.com/mytestextension/mte_v2.crx' version='1.2.0' />
+  </app>
+</gupdate>
+`;
 
 describe('Chrome Extension Fetcher', () => {
   it('instanciate as a singleton', () => {
@@ -26,12 +37,29 @@ describe('Chrome Extension Fetcher', () => {
 
   it('fetch a chrome extension', async () => {
     const cxFetcher = new CxFetcher();
-    const actualCxPath = await cxFetcher.fetchOne(EXTENSION_ID);
+    const cxInfos = await cxFetcher.fetch(EXTENSION_ID);
 
     // Check it arrived at the right place
     assert.equal(
-      actualCxPath,
+      cxInfos.path,
       `/Users/mikael/Documents/Code/electron-chrome-extension/src/browser/cx-fetcher/extensions/${EXTENSION_ID}/8.4.2`
     );
+  });
+
+  it('can check if an update is available', async () => {
+    // Mock downloader to return fake XML
+    const mockDownloader = new class {
+      // @ts-ignore
+      fetchUpdateManifest(url: string) { return FAKE_UPDATE_XML; }
+    };
+
+    // @ts-ignore // TODO : Replace this with something else
+    delete CxFetcher._instance;
+
+    // @ts-ignore
+    const cxFetcher = new CxFetcher(undefined, mockDownloader);
+    cxFetcher.saveCx(EXTENSION_ID, FAKE_CX_INFOS);
+    const actual = await cxFetcher.checkForUpdate(EXTENSION_ID);
+    assert.equal(actual, true);
   });
 });
