b032e0b662ebfd507a1829edb379c73589f4d9fb
Mikaël <lorack@gmail.com>
2018-12-18 18:08:17 +0100

    Refine and implement last parts of storage and download providers

--- src/browser/cx-fetcher/cx-storage-provider.ts
+++ src/browser/cx-fetcher/cx-storage-provider.ts
@@ -1,7 +1,7 @@
 import {
   CxStorageProviderInterface,
   CxManifest,
-  CxInfos,
+  DownloadDescriptor,
 } from './types';
 import * as glob from 'glob';
 const path = require('path');
@@ -22,27 +22,29 @@ class CxStorageProvider implements CxStorageProviderInterface {
    * @param extensionId   Extension ID used for the final folder path
    * @param crxPath       Path of the CRX archive, as a string
    */
-  async extractExtension(extensionId: string, crxPath: string): Promise<CxInfos> {
+  async installExtension(extensionId: string, crxDownload: DownloadDescriptor) {
     try {
-
-      // TODO : this should rather be called "installExtension"
       // TODO : ensureDir at some point
 
-      const rootExtensionFolder = this.getExtensionFolder();
+      const rootExtensionFolder = this.getExtensionsFolder();
       // Extract temporarily in a sub temporary folder
+      // TODO : Improve how is created the temporary folder
       const tempDestination = path.resolve(rootExtensionFolder, TEMP_SORT_FOLDER, extensionId);
-      await this.unzipCrx(crxPath, tempDestination);
+      await this.unzipCrx(crxDownload.path, tempDestination);
 
       // Find version in manifest and create a new destination subfolder
-      const cxInfos = await this.squeezeManifest(`${tempDestination}/manifest.json`);
-      const versionDestination = path.resolve(rootExtensionFolder, extensionId, cxInfos.version);
+      const manifest = await this.readManifest(path.join(tempDestination, 'manifest.json'));
+      const versionDestination = path.resolve(rootExtensionFolder, extensionId, manifest.version);
 
       // TODO : check if the destination already exists and fall back (with cleanup)
       // Move the extracted file to the final versionned folder
       await fse.move(tempDestination, versionDestination);
 
-      // Return gathered cx infos
-      return cxInfos;
+      // Return Installation Infos
+      return {
+        path: versionDestination,
+        manifest,
+      };
 
     } catch (err) {
       throw new Error(err);
@@ -51,8 +53,8 @@ class CxStorageProvider implements CxStorageProviderInterface {
 
   // Gather all installed Cx Infos from installation folder
   async getInstalledExtension() {
-    const installedCxInfos = {};
-    const installationFolder = this.getExtensionFolder();
+    const installedCxInfos = new Map();
+    const installationFolder = this.getExtensionsFolder();
     const cxFolders = glob.sync(path.join(installationFolder, '**/manifest.json'));
 
     for (const manifestPath of cxFolders) {
@@ -60,23 +62,25 @@ class CxStorageProvider implements CxStorageProviderInterface {
       const version = extensionTree[extensionTree.length - 2];
       const extensionId = extensionTree[extensionTree.length - 3];
 
-      if (!installedCxInfos[extensionId]) installedCxInfos[extensionId] = {};
-      installedCxInfos[extensionId][version] = await this.squeezeManifest(manifestPath);
+      if (!installedCxInfos[extensionId]) installedCxInfos.set(extensionId, new Map());
+
+      const manifest = await this.readManifest(manifestPath);
+      // TODO : This is a bit ugly
+      installedCxInfos.get(extensionId).set(version, {
+        path: manifestPath.slice(0, -14),
+        manifest,
+      });
     }
 
-    // Return all CxInfos found under their version / extension ID
+    // Return all Installation Infos found under their version / extension ID
     return installedCxInfos;
-  }
-
-  async removeExtension(extensionId: string) {
-    return 'LET US DO THIS';
   }
 
   /**
    * Return destination folder path to store chrome extensions.
    * @returns   String representing the folder path where the extensions will be extracted
    */
-  getExtensionFolder(): string {
+  getExtensionsFolder(): string {
     return path.resolve(ROOT_FOLDER, EXTENSION_FOLDER);
   }
 
@@ -87,17 +91,6 @@ class CxStorageProvider implements CxStorageProviderInterface {
    */
   async readManifest(manifestPath: string): Promise<CxManifest> {
     return fse.readJson(manifestPath);
-  }
-
-  // TODO : Rename this (ahem)
-  async squeezeManifest(manifestPath: string): Promise<CxInfos> {
-    const manifest = await this.readManifest(manifestPath);
-    const cxInfos = {
-      path: manifestPath.slice(0, -14),   // TODO : This is a bit ugly
-      version: manifest.version,
-      update_url: manifest.update_url,
-    };
-    return cxInfos;
   }
 
   /**
