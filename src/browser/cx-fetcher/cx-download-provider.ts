4038faf11309027f45b8cd4f1b221dd5e7b1888e
Mikaël <lorack@gmail.com>
2018-12-18 18:08:17 +0100

    Refine and implement last parts of storage and download providers

--- src/browser/cx-fetcher/cx-download-provider.ts
+++ src/browser/cx-fetcher/cx-download-provider.ts
@@ -1,10 +1,13 @@
-// download-crx is capble to download a .CRX from WebStore repository
 // @ts-ignore
 import * as downloadCrx from 'download-crx';
+import fetch from 'electron-fetch';
 // @ts-ignore
 import * as tmp from 'tmp';
 const fse = require('fs-extra');
-import { CxDownloadProviderInterface } from './types';
+import {
+  CxDownloadProviderInterface,
+  CxInfos,
+} from './types';
 
 class CxDownloadProvider implements CxDownloadProviderInterface {
   // Track what have been downloaded for cleanups
@@ -17,17 +20,28 @@ class CxDownloadProvider implements CxDownloadProviderInterface {
   async downloadById(extensionId: string) {
     const tempDir = tmp.dirSync().name;
     this.downloads.set(extensionId, tempDir);
-    return downloadCrx.downloadById(extensionId, tempDir, extensionId);
+    return {
+      path: downloadCrx.downloadById(extensionId, tempDir, extensionId),
+    };
   }
 
   async cleanupById(extensionId: string) {
     const tmpDir = this.downloads.get(extensionId);
     fse.remove(tmpDir);
   }
-  
-  async fetchUpdateManifest(updateUrl: string) {
-    console.log(`Fetching ${updateUrl}`);
-    return 'AH AH AH';
+
+  async getUpdateInfo(cxInfos: CxInfos) {
+    console.log(`Fetching ${cxInfos.update_url}`);
+    const res = await fetch(cxInfos.update_url);
+    console.log('res : ', res);
+
+    // @ts-ignore
+    if (!res.ok) throw new Error(`Http Status not ok: ${res.httpStatus}`);
+    const xml = await res.text();
+
+    return {
+      xml,
+    };
   }
 }
 
