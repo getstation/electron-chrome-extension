b1cca46e04b76bc167e54a857fa1d003a0589c1e
Hugo Mano <hugo@hugomano.com>
2018-12-27 10:44:42 +0100

    override window guest

--- src/renderer/init.js
+++ src/renderer/init.js
@@ -29,5 +29,19 @@ if (protocol === `${constants.EXTENSION_PROTOCOL}:`) {
     delete global.global
   })
 } else {
+  // native window open workaround
+  const { ipcRenderer } = require('electron');
+
+  const { guestInstanceId, openerId } = process;
+  const hiddenPage = process.argv.includes('--hidden-page');
+  const usesNativeWindowOpen = process.argv.includes('--native-window-open');
+
+  // Any URL that shouldn't be loaded as `nativeWindowOpen` as a popup
+  // should appear here if parent window uses `nativeWindowOpen`
+  const overrideNativeWindowOpenList = [];
+
+  require('./window-setup')(window, ipcRenderer, guestInstanceId, openerId, hiddenPage, usesNativeWindowOpen, overrideNativeWindowOpenList);
+  // end workaround
+
   require('./injectors/content-scripts-injector')
 }
