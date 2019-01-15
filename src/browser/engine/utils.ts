f4d35f779b4485e7f9f562d7c3f6d8e917d1a78f
Hugo Mano <hugo@hugomano.com>
2019-01-15 11:33:57 +0100

    Remove engine part

--- src/browser/engine/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Protocol } from '../../common';
-
-export const isWindowOrWebView = (webContents: Electron.WebContents) => {
-  // @ts-ignore : missing type
-  const type = webContents.getType();
-  return type === 'window' || type === 'webview';
-};
-
-export const protocolAsScheme = (protocol: Protocol) => protocol.slice(0, -1);
