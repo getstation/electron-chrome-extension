88ca04d3357cc5a07001924d71a4701cb29b2dff
Hugo Mano <hugo@hugomano.com>
2019-05-16 16:31:24 +0200

    fix: remove web_accessible_resources filter and async read for manifest

--- src/browser/engine/protocol.ts
+++ src/browser/engine/protocol.ts
@@ -1,15 +1,18 @@
 import { app, protocol } from 'electron';
-import { readFileSync, createReadStream } from 'fs';
-import { Transform, PassThrough } from 'stream';
+import { createReadStream, readFile } from 'fs';
+import { PassThrough } from 'stream';
 import { lookup } from 'mime-types';
-import { join, dirname, normalize } from 'path';
-import { parse, URL } from 'url';
+import { join } from 'path';
+import { parse } from 'url';
+import { promisify } from 'util';
 // @ts-ignore no types
 import matchAll from 'string.prototype.matchall';
 
 import { Protocol } from '../../common';
 import { getExtensionById } from '../chrome-extension';
 import { protocolAsScheme, fromEntries } from '../../common/utils';
+
+const asyncReadFile = promisify(readFile);
 
 /**
  * Protocol.ts
@@ -34,49 +37,6 @@ const defaultContentSecurityPolicy = 'script-src \'self\' blob: filesystem: chro
 // The protocol handler load file into Buffers
 // before transform them into a Stream (expected in callback).
 // Stream callback allow custom headers.
-//
-// Handlable resources are listed in the extension manifest
-// for a navigation from a **web origin** to an extension resource
-// Allowed HTML pages served under `chrome-extension://` protocol
-// can include extension assets that don't need to be whitelisted.
-// Since we didn't know the origin of the requests*, each time we serve
-// a response, we scan the content to whitelist inner resources.
-//
-// * protocol request referrer is always blank.
-// issue: https://github.com/electron/electron/issues/14747
-//
-// refs:
-// https://developer.chrome.com/extensions/manifest/web_accessible_resources
-// AllowExtensionResourceLoad - https://cs.chromium.org/chromium/src/extensions/browser/extension_protocols.cc?l=429
-const whitelistedResourcesFromProtocolServedFiles = new Set();
-
-// Regex to capture the src attribute value for scrips, stylesheets...
-const resourceLinkRegex = /src\s*=\s*['"](.+?)['"]/g;
-
-const captureWhitelistableResourcesFromProtocolServedFile = (pathname: string) =>
-  new Transform({
-    transform(chunk: any, _encoding: any, cb: Function) {
-      const linksAllowed = matchAll(chunk.toString('utf8'), resourceLinkRegex);
-      const directory = dirname(pathname);
-
-      for (const [, link] of linksAllowed) {
-        try {
-          const isUrl = new URL(link);
-          if (isUrl) break;
-        } catch (e) { }
-
-        const fullPath = join(directory, normalize(link));
-
-        if (!whitelistedResourcesFromProtocolServedFiles.has(fullPath)) {
-          whitelistedResourcesFromProtocolServedFiles.add(fullPath);
-        }
-      }
-
-      // tslint:disable-next-line: no-invalid-this
-      this.push(chunk);
-      cb();
-    },
-  });
 
 const protocolHandler = async (
   { url }: Electron.RegisterBufferProtocolRequest,
@@ -91,7 +51,7 @@ const protocolHandler = async (
   const { src, backgroundPage: { name, html } } = extension;
 
   const manifestPath = join(src, 'manifest.json');
-  const manifestFile = await readFileSync(manifestPath, 'utf-8');
+  const manifestFile = await asyncReadFile(manifestPath, 'utf-8');
   const manifest = JSON.parse(manifestFile);
 
   const headers = new Map();
@@ -121,7 +81,6 @@ const protocolHandler = async (
     headers.set('content-type', 'text/html');
 
     const backgroundPageData = (new PassThrough())
-      .pipe(captureWhitelistableResourcesFromProtocolServedFile(pathname))
       .end(html);
 
     return callback({
@@ -131,22 +90,8 @@ const protocolHandler = async (
     });
   }
 
-  const accessibleResources = manifest.web_accessible_resources
-    .map((r: any) => `/${r}`); // accessibles ressources are relatives to the extension folder. Make them absolut to play with URL Node module
-
-  const isResourceAccessible = accessibleResources.includes(pathname) ||
-    whitelistedResourcesFromProtocolServedFiles.has(pathname);
-
-  if (!isResourceAccessible) {
-    return callback({
-      statusCode: 403,
-      headers: fromEntries(headers),
-    });
-  }
-
   const uri = join(src, pathname);
-  const data = createReadStream(uri)
-    .pipe(captureWhitelistableResourcesFromProtocolServedFile(pathname));
+  const data = createReadStream(uri);
 
   const mimeType = lookup(pathname);
   if (mimeType) headers.set('content-type', mimeType);
